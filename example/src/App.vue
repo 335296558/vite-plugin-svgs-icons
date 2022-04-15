2c6a0aadbebb61451069353d07638c6e16e8c9db
shuangxi <double8295@gmail.com>
2022-04-15 13:56:42 +0700

    flx_bug

--- example/src/App.vue
+++ example/src/App.vue
@@ -14,7 +14,7 @@ const color = ref('#f00')
             针对<a href="https://www.iconfont.cn/" target="_blank">iconfont.cn</a> 
             上的svg 写了一个可以修改color、size vue组件
         </h4>
-        <svgIcon name="test1" :color="color" :size="false"></svgIcon>
+        <svgIcon name="test1" :color="color" :size="size"></svgIcon>
         <svgIcon name="aa" :color="color" :size="size"></svgIcon>
         <svgIcon name="logo" :color="color" :size="size"></svgIcon>
         <svgIcon name="shop" :color="color" :size="size"></svgIcon>
