0dd07c2cfa90a9069b6a39a61d3c6b95b250028f
shuangxi <double8295@gmail.com>
2023-04-14 16:40:33 +0700

    flx_change_vite

---
+++ demo/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vitePluginVueSvgIcons from '../package/dist/index.js';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vitePluginVueSvgIcons()],
+})
--- example/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-// import vue3SvgIcon from 'vue3-svg-icon'
-// console.log(kuiSvgIcon, 'kuiSvgIcon')
-const VueApp = createApp(App);
-// VueApp.component('vue3SvgIcon', vue3SvgIcon)
-VueApp.mount('#app')
