c3d1837360f32537c78e41ed20d5646c0c868458
shuangxi <double8295@gmail.com>
2023-05-12 10:54:26 +0700

    flx_

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,32 +1,91 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+const color = ref('#ff9c00');
+const size = ref(128);
 import svgIcon from 'svg-icon';
 </script>
 
 <template>
-  <div>
-    <svgIcon name="bb" color="#f00" size="90" />
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div>
+        <h1>svg-icon</h1>
+        <h4>
+            针对<a href="https://www.iconfont.cn/" target="_blank">iconfont.cn</a> 
+            上的svg 写了一个可以修改color、size vue组件
+        </h4>
+        <h4 style="color:rgb(253, 169, 169)">
+            多色图标不支持修改color
+        </h4>
+        <!-- <svgIcon name="test1" :color="color" :size="size"></svgIcon>
+        <svgIcon name="aa" :color="color" :size="size"></svgIcon> -->
+        <svgIcon name="logo" :color="color" :size="size"></svgIcon>
+        <svgIcon name="shop" :color="color" :size="size"></svgIcon>
+        <svgIcon name="voice" :color="color" :size="size"></svgIcon>
+        <svgIcon name="testColor" :size="size"></svgIcon>
+        <svgIcon name="moer" :size="size"></svgIcon>
+        <svgIcon name="customerService" :color="color" :size="size"></svgIcon>
+        <svgIcon name="chat" :color="color" :size="size"></svgIcon>
+        <div class="box">
+            <div class="title"><b>修改大小</b></div>
+            <br />
+            <label class="label">
+                <span>大小40</span>
+                <input type="radio" value="40" v-model="size" />
+            </label>
+            <label class="label">
+                <span>大小60</span>
+                <input type="radio" value="60" v-model="size" />
+            </label>
+            <label class="label">
+                <span>大小120</span>
+                <input type="radio" value="120" v-model="size" />
+            </label>
+            <label class="label">
+                <span>大小168</span>
+                <input type="radio" value="168" v-model="size" />
+            </label>
+        </div>
+        <div class="box">
+            <div class="title"><b>修改color</b></div>
+            <br />
+            <label class="label">
+                <span style="color: #000000">黑色</span>
+                <input type="radio" value="#000000" v-model="color" />
+            </label>
+            <label class="label">
+                <span style="color: #ff9c00">黄色</span>
+                <input type="radio" value="#ff9c00" v-model="color" />
+            </label>
+            <label class="label">
+                <span style="color: #0739ff">蓝色</span>
+                <input type="radio" value="#0739ff" v-model="color" />
+            </label>
+            <label class="label">
+                <span style="color: #9707ff">紫色</span>
+                <input type="radio" value="#9707ff" v-model="color" />
+            </label>
+        </div>
+    </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.box {
+    padding: 30px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.label {
+    padding: 15px;
+    font-size: 14px;
+    cursor: pointer;
+    font-weight: 700;
+}
+.icon {
+    font-size: 200px;
 }
 </style>
