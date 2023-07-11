22e1bf3620d689bdad09e505ba87148249763a2d
shuangxi <double8295@gmail.com>
2023-07-11 21:29:40 +0700

    flx_优化

--- package/vite.config.js
+++ package/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
         lib: {
             entry: {
                 index: resolve(__dirname, 'src/index.js'),
-                'components/svgIcon': resolve(__dirname, 'src/components/svgIcon.js')
+                // 'components/svgIcon': resolve(__dirname, 'src/components/svgIcon.js')
             },
             name: 'vitePluginVueSvgIcons',
             // fileName: 'index', // 如果开启所以打包的文件都会变成index，但不会重复，它会index.js、index2.js
